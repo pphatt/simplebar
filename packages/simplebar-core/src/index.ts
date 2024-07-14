95daf84e3f5cb93b229fd9091fd22b2ffda6e22b
pphatt <vutienphat0809@gmail.com>
2024-07-14 17:36:21 +0700

    feat(simplebar-core): increase stopScrollDelay time

--- packages/simplebar-core/src/index.ts
+++ packages/simplebar-core/src/index.ts
@@ -91,7 +91,7 @@ export default class SimpleBarCore {
   draggedAxis?: Axis;
   removePreventClickId: null | number = null;
   minScrollbarWidth = 20;
-  stopScrollDelay = 175;
+  stopScrollDelay = 300;
   isScrolling = false;
   isMouseEntering = false;
   isDragging = false;
